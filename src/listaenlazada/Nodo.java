4b99a6f3e751e52cf1978f4779c86b41785952d6
Jesús Valeta <jvaleta1995@gmail.com>
2023-01-27 11:10:20 -0500

    Agregando repositorios a git

---
+++ src/listaenlazada/Nodo.java
@@ -0,0 +1,21 @@
+package listaenlazada;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Nodo {
+    String dato;
+    Nodo siguiente;
+    
+    public Nodo(){
+        dato="";
+        siguiente=null;
+    }
+    
+    public Nodo(String dat){
+        dato=dat;
+        siguiente=null;
+    }
+    
+}
